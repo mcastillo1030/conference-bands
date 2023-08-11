8921976513c221e1d22939ec2350858fa423b3e7
Marlon Castillo <58896443+mcastillo1030@users.noreply.github.com>
2023-08-11 11:36:39 -0400

    more error fix

--- resources/views/components/guest-input-error.blade.php
+++ resources/views/components/guest-input-error.blade.php
@@ -1,5 +1,5 @@
 @props(['for'])
 
 @error($for)
-    <p {{ $attributes->merge(['class' => 'absolute z-[2] top-full before:absolute before:content-[""] before:top-0 before:left-1/2 before:border-l-4 before:border-l-transparent before:border-r-4 before:border-r-transparent before:border-b-8 before:border-b-ash-400 px-4 py-2 bg-ash-400 before:-translate-x-1/2 before:-translate-y-full rounded-md shadow text-sm text-tangerine-600 dark:text-tangerine-300']) }}>{{ $message }}</p>
+    <p {{ $attributes->merge(['class' => 'absolute z-[2] top-full before:absolute before:content-[""] before:top-0 before:left-1/2 before:border-l-4 before:border-l-transparent before:border-r-4 before:border-r-transparent before:border-b-8 before:border-b-ash-400 px-4 py-2 bg-ash-400 before:-translate-x-1/2 before:-translate-y-full rounded-md shadow text-sm text-tangerine-600']) }}>{{ $message }}</p>
 @enderror
